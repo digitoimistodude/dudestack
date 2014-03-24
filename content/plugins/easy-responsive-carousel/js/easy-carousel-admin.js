4fef5ea4770ea3b0998228c1562c6f76b460e101
Roni Laukkarinen <roni@laukkarinen.info>
2014-03-24 10:30:02 +0200

    composer update

--- content/plugins/easy-responsive-carousel/js/easy-carousel-admin.js
+++
@@ -1,8 +0,0 @@
-/*jslint browser: true*/
-/*global $, jQuery*/
-jQuery(document).ready(function ($) {
-    "use strict";
-    if (typeof jQuery.wp === 'object' && typeof jQuery.wp.wpColorPicker === 'function') {
-        $("#content_color").wpColorPicker();
-    }
-});
--- content/plugins/posts-to-posts/admin/field.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-interface P2P_Field {
-	function get_title();
-	function render( $p2p_id, $item );
-}
-
