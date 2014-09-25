0e7c4b66edbf4ba7f7bee0fc48935187f20fe405
Roni Laukkarinen <roni@laukkarinen.info>
2014-09-25 14:18:46 +0300

    add package.json generate snippet (accidentally removed earlier)

--- createproject.sh
+++ createproject.sh
@@ -641,4 +641,41 @@ RewriteRule . /index.php [L]
 chmod 777 .htaccess
 echo "${yellow}Setting uploads permissions...:${txtreset}"
 chmod -Rv 777 "$HOME/Projects/$PROJECTNAME/content/uploads"
+
+echo "${yellow}Setting up package.json${txtreset}"
+echo "{
+  \"name\": \"$PROJECTNAME\",
+  \"version\": \"1.0.0\",
+  \"description\": \"$PROJECTNAME powered by wpstack-rolle + gulp\",
+  \"author\": \"Digitoimisto Dude Oy (moro@dude.fi)\",
+  \"devDependencies\": {
+    \"browser-sync\": \"~0.8.2\",
+    \"gulp\": \"^3.8.0\",
+    \"gulp-changed\": \"^0.4.1\",
+    \"gulp-imagemin\": \"^0.6.2\",
+    \"gulp-notify\": \"^1.4.2\",
+    \"gulp-ruby-sass\": \"^0.7.1\",
+    \"gulp-compass\": \"^1.2.0\",
+    \"gulp-util\": \"^3.0.0\",
+    \"gulp-minify-css\": \"0.3.7\",
+    \"gulp-autoprefixer\": \"0.0.8\",
+    \"gulp-combine-media-queries\": \"0.1.0\",
+    \"gulp-uglify\": \"0.3.1\",
+    \"gulp-cache\": \"0.2.0\",
+    \"gulp-concat\": \"2.3.4\",
+    \"gulp-header\": \"1.1.1\",
+    \"normalize-css\": \"2.3.1\",
+    \"gulp-pixrem\": \"^0.1.1\",
+    \"require-dir\": \"^0.1.0\",
+    \"psi\": \"^0.1.2\"
+  },
+  \"dependencies\": {
+    \"backbone\": \"~1.1.2\",
+    \"jquery\": \"~2.1.0\"
+  }
+}" > "$HOME/Projects/$PROJECTNAME/package.json"
+
+echo "${yellow}Installing local node.js packages (may take a while)${txtreset}"
+npm install
+
 echo "${boldgreen}All done! Start coding at http://${PROJECTNAME}.dev! Remember to make a repo on Bitbucket, eventually.${txtreset}"
