61781325aacb0a13ea46a8e86ba435128a593273
Roni Laukkarinen <roni@laukkarinen.info>
2015-07-09 22:48:00 +0300

    Missing parenthesis

--- config/application.php
+++ config/application.php
@@ -8,7 +8,7 @@ $dotenv = new Dotenv\Dotenv($root_dir);
 if (file_exists($root_dir . '/.env')) {
   $dotenv->load();
 }
-$dotenv->required(array('DB_NAME', 'DB_USER', 'DB_PASSWORD', 'WP_HOME', 'WP_SITEURL');
+$dotenv->required(array('DB_NAME', 'DB_USER', 'DB_PASSWORD', 'WP_HOME', 'WP_SITEURL'));
 
 /**
  * Set up our global environment constant and load its config first
