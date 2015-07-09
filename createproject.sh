92bf78b273ab7a217e4aa5fcf434c43dd2ccdda8
Roni Laukkarinen <roni@laukkarinen.info>
2015-07-09 22:50:26 +0300

    Remove leftover application.php generate-command
    Why it was even there? it comes with the repo. Silly me.

--- createproject.sh
+++ createproject.sh
@@ -220,68 +220,6 @@ cd "$HOME/Projects/$PROJECTNAME/"
 #rm CONTRIBUTING.md
 rm README.md
 #rm LICENSE.md
-echo "<?php
-\$root_dir = dirname(dirname(__FILE__));
-
-/**
- * Use Dotenv to set required environment variables and load .env file in root
- */
-Dotenv::load(\$root_dir);
-Dotenv::required(array('DB_NAME', 'DB_USER', 'DB_PASSWORD', 'WP_HOME', 'WP_SITEURL'));
-
-/**
- * Set up our global environment constant and load its config first
- * Default: development
- */
-define('WP_ENV', getenv('WP_ENV') ? getenv('WP_ENV') : 'development');
-
-\$env_config = dirname(__FILE__) . '/environments/' . WP_ENV . '.php';
-
-if (file_exists(\$env_config)) {
-  require_once \$env_config;
-}
-
-/**
- * Custom Content Directory
- */
-define('CONTENT_DIR', '/content');
-define('WP_CONTENT_DIR', \$root_dir . CONTENT_DIR);
-define('WP_CONTENT_URL', WP_HOME . CONTENT_DIR);
-
-/**
- * DB settings
- */
-define('DB_CHARSET', 'utf8');
-define('DB_COLLATE', '');
-\$table_prefix = 'wp_';
-
-/**
- * Authentication Unique Keys and Salts
- */
-define('AUTH_KEY',         getenv('AUTH_KEY'));
-define('SECURE_AUTH_KEY',  getenv('SECURE_AUTH_KEY'));
-define('LOGGED_IN_KEY',    getenv('LOGGED_IN_KEY'));
-define('NONCE_KEY',        getenv('NONCE_KEY'));
-define('AUTH_SALT',        getenv('AUTH_SALT'));
-define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
-define('LOGGED_IN_SALT',   getenv('LOGGED_IN_SALT'));
-define('NONCE_SALT',       getenv('NONCE_SALT'));
-
-/**
- * Custom Settings
- */
-define('AUTOMATIC_UPDATER_DISABLED', true);
-define('DISABLE_WP_CRON', false);
-define('DISALLOW_FILE_EDIT', true);
-
-/**
- * Bootstrap WordPress
- */
-if (!defined('ABSPATH')) {
-  define('ABSPATH', \$root_dir . '/wp/');
-}
-" > config/application.php
-echo "${boldgreen}application.php (modern wp-config) generated${txtreset}"
 echo "${yellow}Updating WordPress related stuff...:${txtreset}"
 cp $HOME/Projects/dudestack/composer.json "$HOME/Projects/$PROJECTNAME/composer.json"
 cd "$HOME/Projects/$PROJECTNAME/"
