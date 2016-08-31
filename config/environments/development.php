0ef22f18dc3049b37b7f0e731cbdab56f047f04e
Timi Wahalahti <timi.wahalahti@gmail.com>
2016-08-31 12:05:52 +0300

    Turn WP_DEBUG on by default in development env

--- config/environments/development.php
+++ config/environments/development.php
@@ -9,6 +9,6 @@ define('WP_HOME', getenv('WP_HOME'));
 define('WP_SITEURL', getenv('WP_SITEURL'));
 
 define('SAVEQUERIES', true);
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', false);
 define('FS_METHOD', 'direct');
