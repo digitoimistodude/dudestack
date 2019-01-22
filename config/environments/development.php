c06f5af2cee7a802054ee92eec7803fc5ae2f330
Timi Wahalahti <timi.wahalahti@gmail.com>
2019-01-22 17:31:11 +0200

    Disable WSOD protection in development

--- config/environments/development.php
+++ config/environments/development.php
@@ -8,9 +8,11 @@ define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost');
 define('WP_HOME', getenv('WP_HOME'));
 define('WP_SITEURL', getenv('WP_SITEURL'));
 
+define('FS_METHOD', 'direct');
+
 define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
-define('FS_METHOD', 'direct');
+define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
 
 define( 'PLL_CACHE_HOME_URL', false );
