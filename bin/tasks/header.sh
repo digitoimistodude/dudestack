9c5cb0fe37c8dda49e94eee8909ae66c9d67cba3
Roni Laukkarinen <roni@dude.fi>
2021-04-12 18:20:14 +0300

    Cleaner header.sh

--- bin/tasks/header.sh
+++ bin/tasks/header.sh
@@ -3,19 +3,16 @@ echo "-----------------------------------------------------"
 echo "createproject start script ${SCRIPT_LABEL}, v${SCRIPT_VERSION}"
 echo "-----------------------------------------------------"
 echo ""
-if [ $0 != '/usr/local/bin/createproject' ]; then
-echo "${TXTBOLD}Please note: ${TXTRESET}${WHITE}Please do NOT prepend sh or bash to run this script. Run this script directly instead."
-echo ""
-echo "Wrong: ${RED}sh $CURRENTFILE${WHITE} or ${RED}bash $CURRENTFILE${WHITE}"
-echo "Right: ${GREEN}./$CURRENTFILE${WHITE} (if no executable permissions, run ${GREEN}sudo chmod +x $CURRENTFILE first)${WHITE}" 1>&2
-echo ""
-fi
 if [ ! -f /usr/local/bin/createproject ]; then
-echo "${TXTRESET}${TXTBOLD}Preferred way to run:${TXTRESET}${WHITE} Link this file to system level with this command:${TXTRESET}"
+echo "${TXTRESET}${TXTBOLD}ACTION REQUIRED:${TXTRESET}${WHITE} Link this file to system level and start from there with this oneliner:${TXTRESET}"
 echo ""
-echo "${GREEN}sudo ln -s ${DIR_TO_FILE}${CURRENTFILE} /usr/local/bin/createproject${TXTRESET}" 1>&2
+echo "${GREEN}sudo ln -s ${DIR_TO_FILE}${CURRENTFILE} /usr/local/bin/createproject && sudo chmod +x /usr/local/bin/createproject && createproject${TXTRESET}" 1>&2
 echo ""
-echo "After this you can just run:"
-echo "${GREEN}createproject${TXTRESET}"
+exit
+fi
+if [ $0 != '/usr/local/bin/createproject' ]; then
+echo "${TXTRESET}${WHITE}Please do NOT run this script with ${RED}sh $CURRENTFILE${WHITE} or ${RED}bash $CURRENTFILE${WHITE} or ${RED}./$CURRENTFILE${WHITE}.
+Run this script globally instead by simply typing: ${GREEN}createproject${TXTRESET}"
 echo ""
+exit
 fi
