5201a2e13a1456a919bd88d3fd546b7f088b4669
Roni Laukkarinen <roni@dude.fi>
2021-04-12 20:27:32 +0300

    Remove duplicate filemode

--- bin/tasks/permissions.sh
+++ bin/tasks/permissions.sh
@@ -1,4 +1,2 @@
 echo "${YELLOW}Setting file permissions for local dev server...${TXTRESET}"
 sudo chmod -R 777 $PROJECTS_HOME/$PROJECTNAME
-cd $PROJECTS_HOME/$PROJECTNAME
-git config core.fileMode false
