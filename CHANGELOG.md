9e353d89eb7eb241bc451bdc51917fb5c8c40ab8
Roni Laukkarinen <roni@dude.fi>
2021-08-24 13:38:11 +0300

    Fix GitHub repository init in start script

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+### 2.1.1: 2021-08-24
+
+* Fix GitHub repository init in start script
+
 ### 2.1.0: 2021-06-21
 
 * Bump WordPress to 5.7.2
--- bin/tasks/github.sh
+++ bin/tasks/github.sh
@@ -4,18 +4,23 @@ ENV_FILE="${HOME}/.env_createproject"
 
 # Check if .env  entry exists
 if grep -q "GITHUB_COMPANY_USERNAME" ${ENV_FILE}; then
+
+  # Let's make sure the vars are found
+  GITHUB_ACCESS_TOKEN_ENV=$(grep GITHUB_ACCESS_TOKEN $ENV_FILE | cut -d '=' -f2)
+  GITHUB_COMPANY_USERNAME_ENV=$(grep GITHUB_COMPANY_USERNAME $ENV_FILE | cut -d '=' -f2)
+
   # If found
   echo ""
   echo "${YELLOW}Creating a GitHub repo...${TXTRESET}"
 
-  curl -u "$GITHUB_COMPANY_USERNAME_ENV":"$GITHUB_ACCESS_TOKEN_ENV" https://api.github.com/orgs/"$GITHUB_COMPANY_USERNAME_ENV"/repos -d '{"name": "'${PROJECTNAME}'","auto_init": false, "private": true, "description": "A repository for '${PROJECTNAME}' site"}'
+  curl -u "${GITHUB_COMPANY_USERNAME_ENV}":"${GITHUB_ACCESS_TOKEN_ENV}" https://api.github.com/orgs/"${GITHUB_COMPANY_USERNAME_ENV}"/repos -d '{"name": "'${PROJECTNAME}'","auto_init": false, "private": true, "description": "A repository for '${PROJECTNAME}' site"}'
 
   echo "${YELLOW}Initializing the GitHub repo...${TXTRESET}"
   cd "$PROJECTS_HOME/$PROJECTNAME"
 
   # We ensured earlier in the main script that git is installed so it's safe to run these
   git init
-  git remote add origin git@github.com:$GITHUB_COMPANY_USERNAME/$PROJECTNAME.git
+  git remote add origin git@github.com:${GITHUB_COMPANY_USERNAME_ENV}/${PROJECTNAME}.git
   git config core.fileMode false
   git add --all
   git commit -m 'First commit - project started'
