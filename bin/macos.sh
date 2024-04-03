e32b758d0570a3fba0c0f753f9b086889e6fce94
Roni Laukkarinen <roni@dude.fi>
2024-04-03 17:31:14 +0300

    Bump createproject to 1.1.2 (2024-04-03)

--- bin/macos.sh
+++ bin/macos.sh
@@ -4,7 +4,7 @@
 
 # Script specific vars
 SCRIPT_LABEL='for macOS'
-SCRIPT_VERSION='1.1.1 (2024-03-12)'
+SCRIPT_VERSION='1.1.2 (2024-04-03)'
 
 # Vars needed for this file to function globally
 CURRENTFILE=`basename $0`
